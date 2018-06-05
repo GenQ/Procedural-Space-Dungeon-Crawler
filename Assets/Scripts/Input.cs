659556f8246973fa1bd363c33ef66a6e3382d496
Areos85 <marcusman26@gmail.com>
2018-06-05 11:58:34 -0400

    Its All Working No Bugs

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
+public class Input : MonoBehaviour {
+    private Movement movement;
+
+    private void Start() {
+        movement = GetComponent<Movement>();
+    }
+
+    private void Update() {
+        float x = UnityEngine.Input.GetAxis("Horizontal");
+        float y = UnityEngine.Input.GetAxis("Vertical");
+
+        movement.Move(x, y);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    [SerializeField] private float movementSpeed;
+
+
+    public void Move(float x, float y) {
+        Vector2 translation =  new Vector2(x, y) * movementSpeed * Time.deltaTime;
+        transform.Translate(translation);
+    }
+
+    public void Look(Vector2 dir) {
+        transform.rotation.SetLookRotation(dir);
+    }
+}
